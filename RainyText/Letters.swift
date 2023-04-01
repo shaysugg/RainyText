92da45db7ad8ffebc96c7db8ef86365b3043e0f0
Shayan <shayanbadev@gmail.com>
2023-04-01 16:39:33 +0330

    Separate letters characters and rawvalues

--- RainyText/Letters.swift
+++ RainyText/Letters.swift
@@ -8,13 +8,28 @@
 import Foundation
 
 enum Letters: String, CaseIterable {
-    case english = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    case arabic = "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"
-    case chinese = "我是一个语言模型很高兴为您服务中文是一门非常有趣和复杂的语言要不断的学习和练习才能掌握好希望您能够享受学习中文的过程"
-    case numbers = "1234567890"
-    case symbols = "!@#$%^&*()"
+    case english = "english"
+    case arabic = "arabic"
+    case chinese = "chinese"
+    case numbers = "numbers"
+    case symbols = "symbols"
+    
+    var characters: String {
+        switch self {
+        case .english:
+            return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        case .arabic:
+            return "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"
+        case .chinese:
+            return "我是一个语言模型很高兴为您服务中文是一门非常有趣和复杂的语言要不断的学习和练习才能掌握好希望您能够享受学习中文的过程"
+        case .numbers:
+            return "1234567890"
+        case .symbols:
+            return "!@#$%^&*()"
+        }
+    }
     
     static func generateRandomCharacter(from letters: Set<Letters>) -> String {
-        String(letters.flatMap { $0.rawValue }.randomElement()!)
+        String(letters.flatMap { $0.characters }.randomElement()!)
     }
 }
