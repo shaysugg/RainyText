e07770c138b726d493adb5fdff4afa8dee30824b
Shayan <metrosug@gmail.com>
2023-04-12 23:14:34 +0330

    Hide setting button after a while

--- RainyText/iOSContentView.swift
+++ RainyText/iOSContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct iOSContentView: View {
     @State private var viewID = 0
     @State private var presentSetting = false
+    @State private var isSettingButtonHidden = false
     @EnvironmentObject var setting: Setting
     var body: some View {
         ZStack(alignment: .bottomTrailing) {
@@ -19,18 +20,46 @@ struct iOSContentView: View {
                 .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
                     viewID += 1
                 }
-            Button {
+            SettingButton(isHidden: $isSettingButtonHidden) {
                 presentSetting = true
-            } label: {
-                Image(systemName: "gearshape.fill")
             }
-            .tint(.white)
-            .padding()
-            .statusBarHidden(setting.hideStatusBar)
-            .sheet(isPresented: $presentSetting) {
-                iOSSettingView(setting: setting, isPresented: $presentSetting)
-            }
+        }
+        .statusBarHidden(setting.hideStatusBar)
+        .sheet(isPresented: $presentSetting) {
+            iOSSettingView(setting: setting, isPresented: $presentSetting)
+        }
+        .onTapGesture {
+            isSettingButtonHidden = false
+        }
+    }
+}
 
+
+struct SettingButton: View {
+    @Binding var isHidden: Bool
+    var action: () -> Void
+    
+    var body: some View {
+        Button {
+            action()
+        } label: {
+            Image(systemName: "gearshape.fill")
+                .resizable()
+                .frame(width: 25, height: 25)
+        }
+        .tint(.white)
+        .padding()
+        .opacity(isHidden ? 0 : 1)
+        .animation(.default, value: isHidden)
+        .onAppear(perform: hideWithDelay)
+        .onChange(of: isHidden) { hidden in
+            if !hidden { hideWithDelay() }
+        }
+    }
+    
+    func hideWithDelay() {
+        DispatchQueue.main.asyncAfter(wallDeadline: .now() + 2) {
+            isHidden = true
         }
     }
 }
